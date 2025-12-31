a9f4f4bfdd660acd6be55339f0ca2e7bfbce8e88
xeiNg0de <alekseytilte@gmail.com>
2025-12-31 19:03:39 +0300

    add splitter

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from PySide6.QtWidgets import (
   QWidget,
   QVBoxLayout,
   QLabel,
+  QSplitter,
 )
 from PySide6.QtGui import QPixmap
 
@@ -18,25 +19,44 @@ class MainWindow(QMainWindow):
 
     self.setWindowTitle("VTuberHikka")
     self.resize(600, 400)
-
+    
     central_widget = QWidget()
     self.setCentralWidget(central_widget)
 
     layout = QVBoxLayout()
     central_widget.setLayout(layout)
 
-    self.label = QLabel("Hello world")
-    self.label.setAlignment(Qt.AlignCenter)
+    #splitter
+    self.splitter = QSplitter(Qt.Horizontal)
+    layout.addWidget(self.splitter)
+    
+    #left panel
+    self.left_panel = QWidget()
+    left_layout = QVBoxLayout()
+    self.left_panel.setLayout(left_layout)
 
-    button = QPushButton("Load ")
-    layout.addWidget(button)
-
+    self.label = QLabel()
     pixmap = QPixmap("71.jpg")
     self.label.setPixmap(pixmap)
 
-    layout.addWidget(self.label)
+    left_layout.addWidget(self.label)
+
+    #right panel
+    self.right_panel = QWidget()
+    right_layout = QVBoxLayout()
+    self.right_panel.setLayout(right_layout)
+
+    button = QPushButton("Load ")
+    right_layout.addWidget(button)
 
     button.clicked.connect(self.on_button_click)
+
+    w = self.width()
+    self.splitter.setSizes([int(w*0.8), int(w*0.2)])
+    
+    self.splitter.addWidget(self.left_panel)
+    self.splitter.addWidget(self.right_panel)
+
 
   def on_button_click(self):
     self.label.setText("Кнопка нажата")
