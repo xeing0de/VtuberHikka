3e78e038556669a012a0c486808e01c8a9981309
xeiNg0de <alekseytilte@gmail.com>
2026-01-04 06:11:31 +0300

    change UI

--- panels/right.py
+++ panels/right.py
@@ -7,6 +7,7 @@ from PySide6.QtWidgets import (
         QLabel, 
         QSpinBox, 
         QFormLayout,
+        QGroupBox,
         )
 
 
@@ -21,11 +22,10 @@ class RightPanel(QWidget):
         self.btn_load = QPushButton("Load Project")
         self.btn_save = QPushButton("Save Project")
         self.btn_limage = QPushButton("Load Image")
+        self.btn_text = QPushButton("Add Text")
+        self.btn_start = QPushButton("Start")
 
-        project_row = QHBoxLayout()
-        project_row.addWidget(self.btn_load)
-        project_row.addWidget(self.btn_save)
-
+        #spinboxes
         self.sp_w = QSpinBox()
         self.sp_w.setRange(1, 20000)
         self.sp_w.setValue(500)
@@ -34,17 +34,50 @@ class RightPanel(QWidget):
         self.sp_h.setRange(1, 20000)
         self.sp_h.setValue(500)
 
-        row = QHBoxLayout()
-        row.addWidget(QLabel("Width"))
-        row.addWidget(self.sp_w)
-        row.addWidget(QLabel("Height"))
-        row.addWidget(self.sp_h)
+        #project
+        gb_project = QGroupBox("Project")
+        gb_project_layout = QVBoxLayout(gb_project)
+        gb_project_layout.setContentsMargins(8, 10, 8, 8)
+        gb_project_layout.setSpacing(6)
 
-        layout.addLayout(project_row)
-        layout.addWidget(QLabel("Project name: " + self.project.name))
-        layout.addLayout(row)
-        layout.addWidget(self.btn_limage)
+        project_row = QHBoxLayout()
+        project_row.addWidget(self.btn_load)
+        project_row.addWidget(self.btn_save)
+
+        self.lbl_project_name = QLabel(f"Project name: {self.project.name}")
+
+        gb_project_layout.addLayout(project_row)
+        gb_project_layout.addWidget(self.lbl_project_name)
+
+        #workspace
+        gb_workspace = QGroupBox("Workspace")
+        gb_workspace_layout = QVBoxLayout(gb_workspace)
+        gb_workspace_layout.setContentsMargins(8, 10, 8, 8)
+        gb_workspace_layout.setSpacing(6)
+        
+        workspace_row = QHBoxLayout()
+        workspace_row.addWidget(QLabel("Width"))
+        workspace_row.addWidget(self.sp_w)
+        workspace_row.addWidget(QLabel("Height"))
+        workspace_row.addWidget(self.sp_h)
+
+        gb_workspace_layout.addLayout(workspace_row)
+
+        #toolbar
+        gb_toolbar = QGroupBox("Toolbar")
+        gb_toolbar_layout = QHBoxLayout(gb_toolbar)
+        gb_toolbar_layout.setContentsMargins(8, 10, 8, 8)
+        gb_toolbar_layout.setSpacing(6)
+
+        gb_toolbar_layout.addWidget(self.btn_limage)
+        gb_toolbar_layout.addWidget(self.btn_text)
+
+
+        #compose
+        layout.addWidget(gb_project)
+        layout.addWidget(gb_workspace)
+        layout.addWidget(gb_toolbar)
+
         layout.addStretch()
 
-        self.btn_start = QPushButton("Start")
         layout.addWidget(self.btn_start)
